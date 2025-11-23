27abb456f962323f6b40fa88476814a3ca70b1ac
Nikhil383 <nikhilmahesh89@gmail.com>
2025-11-23 18:32:51 +0530

    modified

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,8 +1,9 @@
+# tests/test_model.py
 from app.model_utils import load_model, predict_text
 
-def test_prediction():
+def test_prediction_labels():
     pipe = load_model(None)
     res = predict_text(pipe, "I love this!")
     assert isinstance(res, list)
-    assert "label" in res[0]
+    assert res[0]["label"] in {"NEGATIVE", "NEUTRAL", "POSITIVE"}
     assert "score" in res[0]
